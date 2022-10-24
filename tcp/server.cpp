6d3fc31b6557a03671eb38d47a827fdbbedc6437
Anastasia Fedorova <79627149+anasfreud@users.noreply.github.com>
2022-10-25 00:18:33 +0300

    Update server.cpp

--- tcp/server.cpp
+++ tcp/server.cpp
@@ -1,82 +1,80 @@
 #include <iostream>
-
-// используется 2-я версия winsock
 #include <winsock2.h>
 
-// linker подключит библиотеку в список библиотечных зависимостей
 #pragma comment (lib, "Ws2_32.lib")
-  
 
 using namespace std;
 
-#define	DEFAULT_PORT 5000
+#define	SERVER_PORT 5000
+
 
 int cleanUp()
 {
-	// освобождение ресурсов, выделенных Winsock
-
 	if (WSACleanup() == SOCKET_ERROR) {
-		cout << "WSACleanup() error " << WSAGetLastError() << '\n';
+		cout << "WSACleanup error: " << WSAGetLastError() << '\n';
 		return 0;
+	} else {
+		return 1;
 	}
-	return 1;
 }
+
 
 int main()
 {
-
 	int iResult;
 
-	// 0) <регистрация приложения в библиотеке>
 	WSAData wsaData;
 
-	if (WSAStartup(0x0202, &wsaData)) { //0x0202 - версия 2.2 интерфейса Winsock
-		cout << "WSAStartup() error " << WSAGetLastError() << '\n';
+	iResult = WSAStartup(0x0202, &wsaData);
+
+	if (iResult) {
+		cout << "WSAStartup error: " << WSAGetLastError() << '\n';
 		cleanUp();
 	}
 
 
-	// 1) <создаем сокет>
-	// AF_INET - протокол IPv4, 
-	// SOCK_STREAM - потоковый режим соединения, 
-	// 0 - протокол соединения по умолчанию (TCP)
+	SOCKET listen_socket = socket(AF_INET, SOCK_STREAM, 0);
 
-	SOCKET s = socket(AF_INET, SOCK_STREAM, 0);
-	if (s == INVALID_SOCKET) {
-		cout << "socket() error " << WSAGetLastError() << '\n';
+	if (listen_socket == INVALID_SOCKET) {
+		cout << "socket error: " << WSAGetLastError() << '\n';
+		closesocket(listen_socket);
 		cleanUp();
 	}
 
+	sockaddr_in s_in;
 
-	// 2) <описываем адрес сокета>
-	sockaddr_in sin;
+	s_in.sin_family = AF_INET;
+	s_in.sin_port = htons(SERVER_PORT);
+	s_in.sin_addr.s_addr = INADDR_ANY;
 
-	sin.sin_family = AF_INET; // задали семейство IP (IPv4)
+	iResult = bind(listen_socket, (sockaddr*) &s_in, sizeof(s_in));
 
-	// htons - преобразовывает данные локального хоста в сетевой формат
-	sin.sin_port = htons(DEFAULT_PORT); // задали порт
+	if (iResult == INVALID_SOCKET) {
+		cout << "bind error: " << WSAGetLastError() << '\n';
+		closesocket(listen_socket);
+		cleanUp();
+	}
 
-	// s_addr ~ S_un.S_addr
-	// INADDDR_ANY - сокет работает с любым IP-адресом
-	sin.sin_addr.s_addr = INADDR_ANY; // задали IP, с которым будет работать сокет.
+	iResult = listen(listen_socket, SOMAXCONN);
+	if (iResult == SOCKET_ERROR) {
+		cout << "listen error: " << WSAGetLastError() << '\n';
+		closesocket(listen_socket);
+		cleanUp();
+	}
 
+	int from_len;
+	sockaddr_in from_s_in;
+	from_len = sizeof(from_s_in);
 
-	// 3) <привязываем адрес к сокету>
-	bind(s, (sockaddr*) &sin, sizeof(sin));
+	SOCKET new_socket;
 
-	listen(s, SOMAXCONN);
+	while (true) {
+		new_socket = accept(listen_socket, (sockaddr*)&from_s_in, &from_len);
+		cout << "New connection\n";
+		closesocket(new_socket);
+	}
 
-	sockaddr addr;
-	int len = sizeof(addr);
-	accept(s, &addr, &len);
-
-
-
-	// n) закрываем сокет
-	closesocket(s);
-
-
-
+	closesocket(listen_socket);
 	cleanUp();
 
 	return 0;
