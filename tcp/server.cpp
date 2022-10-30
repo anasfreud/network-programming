b1857164b356fc31534ef05e047d744afabd7289
Anastasia Fedorova <79627149+anasfreud@users.noreply.github.com>
2022-10-30 18:56:36 +0300

    Update server.cpp

--- tcp/server.cpp
+++ tcp/server.cpp
@@ -1,11 +1,15 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
 #include <iostream>
 #include <winsock2.h>
+#include <string>
 
 #pragma comment (lib, "Ws2_32.lib")
 
 using namespace std;
 
 #define	SERVER_PORT 5000
+#define BUF_SIZE 64
 
 
 int cleanUp()
@@ -29,7 +33,7 @@ int main()
 
 	if (iResult) {
 		cout << "WSAStartup error: " << WSAGetLastError() << '\n';
-		cleanUp();
+		return cleanUp();
 	}
 
 
@@ -38,10 +42,11 @@ int main()
 	if (listen_socket == INVALID_SOCKET) {
 		cout << "socket error: " << WSAGetLastError() << '\n';
 		closesocket(listen_socket);
-		cleanUp();
+		return cleanUp();
 	}
 
 	sockaddr_in s_in;
+	ZeroMemory(&s_in, sizeof(s_in));
 
 	s_in.sin_family = AF_INET;
 	s_in.sin_port = htons(SERVER_PORT);
@@ -52,30 +57,62 @@ int main()
 	if (iResult == INVALID_SOCKET) {
 		cout << "bind error: " << WSAGetLastError() << '\n';
 		closesocket(listen_socket);
-		cleanUp();
+		return cleanUp();
 	}
+
+	cout << inet_ntoa(s_in.sin_addr);
 
 	iResult = listen(listen_socket, SOMAXCONN);
 	if (iResult == SOCKET_ERROR) {
 		cout << "listen error: " << WSAGetLastError() << '\n';
 		closesocket(listen_socket);
-		cleanUp();
+		return cleanUp();
 	}
 
-	int from_len;
+
+
+	SOCKET from_socket;
 	sockaddr_in from_s_in;
-	from_len = sizeof(from_s_in);
 
-	SOCKET new_socket;
+	char buf[BUF_SIZE];
 
+	string msg = "Hello from server!";
+	cout << "Server has started on port " << SERVER_PORT << '\n';
+	
 	while (true) {
-		new_socket = accept(listen_socket, (sockaddr*)&from_s_in, &from_len);
-		cout << "New connection\n";
-		closesocket(new_socket);
+		int from_len = sizeof(from_s_in);
+		from_socket = accept(listen_socket, (sockaddr*)&from_s_in, &from_len);
+
+		
+		cout << "New connection with " << inet_ntoa(from_s_in.sin_addr) << '\n';
+
+		while (true) {
+			send(from_socket, msg.c_str(), msg.size(), 0);
+			cout << "Waiting for request...\n";
+
+			from_len = recv(from_socket, (char*)&buf, BUF_SIZE, 0);
+
+			if ((string)buf == "stop") {
+				break;
+			}
+			
+			cout << "Client requested: ";
+
+			for (int i = 0; i < from_len; i++) {
+				cout << buf[i];
+			}
+			cout << '\n';
+
+			
+			cout << "Please, enter your respond\n";
+			getline(cin, msg);
+		}
+
+		cout << "Connection closed\n";
+		closesocket(from_socket);
 	}
 
 	closesocket(listen_socket);
-	cleanUp();
+	return cleanUp();
 
-	return 0;
 }
